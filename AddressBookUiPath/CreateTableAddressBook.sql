35064965e88b485e95cf0bcc207b2e42a3d147bc
Sudipta <sudiptabala32@gmail.com>
2020-11-28 07:32:21 +0530

    [Sudipta] Add : Addressbook problem

---
+++ AddressBookUiPath/CreateTableAddressBook.sql
@@ -0,0 +1,14 @@
+create table EmpContacts
+(
+FirstName varchar(50) NOT NULL,
+LastName varchar(50) NOT NULL,
+FatherName varchar(50) NOT NULL,
+BloodGroup varchar(50) NOT NULL,
+Address varchar(50) NOT NULL,
+City varchar(50) NOT NULL,
+State varchar(50) NOT NULL,
+Zip varchar(50) NOT NULL,
+Phone varchar(50) NOT NULL,
+Email varchar (50) NOT NULL,
+Primary Key (Phone)
+);
